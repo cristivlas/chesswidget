f6d39a6fed2c48b58f73b6a0bae15a88b4275a1a
Cristian <cristi.vlasceanu@gmail.com>
2020-09-04 14:18:09 -0700

    split class modules

--- ChessWidget.py
+++ ChessWidget.py
@@ -1,12 +1,7 @@
-from kivy.core.image import Image as CoreImage
-from kivy.graphics import *
-from kivy.utils import get_color_from_hex
+from kivy.graphics import Color, Rectangle, InstructionGroup
 from kivy.uix.widget import Widget
 
-import cairosvg
 import chess
-import chess.svg
-import io
 import math
 
 class ChessWidget(Widget):
@@ -19,7 +14,7 @@ class ChessWidget(Widget):
         self.board = chess.Board()
         self.board_pos = None
         self.square_size = None
-        self.xyo = [0, 0]   # xy origin for squares (not the origin of the board, which may include coords)
+        self.xyo = [0, 0]   # xy origin for squares (not the board, which may include coords)
         self.move = str()   # pending move, uci notation
         self.selection = InstructionGroup()
         self.highlight = InstructionGroup()
@@ -100,45 +95,3 @@ class ChessWidget(Widget):
             col, row = 'abcdefgh'.index(col), int(row) - 1
         return [math.ceil(o + i * self.square_size)-1 for o, i in zip(self.xyo, [col, row])]
 
-
-SVG_MARGIN = 15
-
-class SVGChessWidget(ChessWidget):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.piece_tex = {}
-        self.board_tex = None
-        self.margin = SVG_MARGIN
-
-    def board_texture(self):
-        if self.board_tex is None:
-            svg = chess.svg.board(board=None, size=self.board_size)
-            png = cairosvg.svg2png(bytestring=svg)
-            self.board_tex = CoreImage(io.BytesIO(png), ext='png').texture
-        return self.board_tex
-
-    def highlight_area(self, group, i, x, y, w, h):
-        color = ['#aaa23b', '#cdd16a']
-        group.add(Color(*get_color_from_hex(color[i])))
-        if group == self.selection:
-            group.add(Line(points=[x, y, x+w, y, x+w, y+h, x, y+h, x, y], width=3))
-        else:
-            assert group == self.highlight
-            group.add(Rectangle(pos=(x, y), size=(w, h)))
-
-    def piece_texture(self, piece: chess.Piece):
-        tex = self.piece_tex.get(piece.symbol, None)
-        if tex is None:
-            size = self.square_size
-            svg = chess.svg.piece(piece)
-            png = cairosvg.svg2png(bytestring=svg, output_width=size, output_height=size)
-            self.piece_tex[piece.symbol] = tex = CoreImage(io.BytesIO(png), ext='png').texture
-        return tex
-
-    def recalc(self, size):
-        self.board_size = min(size)
-        self.scale = self.board_size / (2 * SVG_MARGIN + 8 * chess.svg.SQUARE_SIZE)
-        self.margin = SVG_MARGIN * self.scale
-        self.square_size = chess.svg.SQUARE_SIZE * self.scale
-        self.board_pos = [(i - self.board_size) / 2 for i in size]
-        self.xyo = [i + self.margin for i in self.board_pos]
---
+++ SVGChessWidget.py
@@ -0,0 +1,52 @@
+from kivy.core.image import Image as CoreImage
+from kivy.graphics import Color, Line, Rectangle
+from kivy.utils import get_color_from_hex
+
+from chesswidget.ChessWidget import ChessWidget
+import cairosvg
+import chess
+import chess.svg
+import io
+
+SVG_MARGIN = 15
+
+
+class SVGChessWidget(ChessWidget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.piece_tex = {}
+        self.board_tex = None
+        self.margin = SVG_MARGIN
+
+    def board_texture(self):
+        if self.board_tex is None:
+            svg = chess.svg.board(board=None, size=self.board_size)
+            png = cairosvg.svg2png(bytestring=svg)
+            self.board_tex = CoreImage(io.BytesIO(png), ext='png').texture
+        return self.board_tex
+
+    def highlight_area(self, group, i, x, y, w, h):
+        color = ['#aaa23b', '#cdd16a']
+        group.add(Color(*get_color_from_hex(color[i])))
+        if group == self.selection:
+            group.add(Line(points=[x, y, x+w, y, x+w, y+h, x, y+h, x, y], width=3))
+        else:
+            assert group == self.highlight
+            group.add(Rectangle(pos=(x, y), size=(w, h)))
+
+    def piece_texture(self, piece: chess.Piece):
+        tex = self.piece_tex.get(piece.symbol, None)
+        if tex is None:
+            size = self.square_size
+            svg = chess.svg.piece(piece)
+            png = cairosvg.svg2png(bytestring=svg, output_width=size, output_height=size)
+            self.piece_tex[piece.symbol] = tex = CoreImage(io.BytesIO(png), ext='png').texture
+        return tex
+
+    def recalc(self, size):
+        self.board_size = min(size)
+        self.scale = self.board_size / (2 * SVG_MARGIN + 8 * chess.svg.SQUARE_SIZE)
+        self.margin = SVG_MARGIN * self.scale
+        self.square_size = chess.svg.SQUARE_SIZE * self.scale
+        self.board_pos = [(i - self.board_size) / 2 for i in size]
+        self.xyo = [i + self.margin for i in self.board_pos]
