bfb0fe883bd383553ed809c9d2f0b00c192f6a92
Cristian <cristi.vlasceanu@gmail.com>
2020-09-08 11:33:38 -0700

    widget that uses kivy atlas + utility

---
+++ AtlasChessWidget.py
@@ -0,0 +1,54 @@
+from kivy.atlas import Atlas
+from kivy.core.image import Image as CoreImage
+from kivy.graphics import Color, Line, Rectangle
+from kivy.utils import get_color_from_hex
+from chesswidget.ChessWidget import ChessWidget
+from os import path
+
+import chess
+import chess.svg
+import sys
+
+SVG_MARGIN = 15
+
+
+class AtlasChessWidget(ChessWidget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.board_tex = None
+        self.margin = SVG_MARGIN
+
+    def set_textures(self, atlas, board):
+        self.atlas = atlas + '.atlas'
+        self.board = board
+        dir = path.dirname(sys.argv[0])
+        if not path.isabs(self.atlas):
+            self.atlas = path.join(dir, self.atlas)
+        if not path.isabs(self.board):
+            self.board = path.join(dir, self.board)
+        self.atlas = Atlas(self.atlas)
+
+    def board_texture(self):
+        if self.board_tex is None:
+            self.board_tex = CoreImage(self.board).texture
+        return self.board_tex
+
+    def highlight_area(self, group, i, x, y, w, h):
+        color = ['#aaa23b', '#cdd16a']
+        group.add(Color(*get_color_from_hex(color[i])))
+        if group == self.selection:
+            group.add(Line(points=[x, y, x+w, y, x+w, y+h, x, y+h, x, y], width=3))
+        else:
+            assert group == self.highlight
+            group.add(Rectangle(pos=(x, y), size=(w, h)))
+
+    def piece_texture(self, piece: chess.Piece):
+        return self.atlas[piece.symbol()]
+
+    def recalc(self, size):
+        self.board_size = min(size)
+        self.scale = self.board_size / (2 * SVG_MARGIN + 8 * chess.svg.SQUARE_SIZE)
+        self.margin = SVG_MARGIN * self.scale
+        self.square_size = chess.svg.SQUARE_SIZE * self.scale
+        self.board_pos = [(i - self.board_size) / 2 for i in size]
+        self.xyo = [i + self.margin for i in self.board_pos]
---
+++ gen_atlas.py
@@ -0,0 +1,39 @@
+import chess
+import chess.svg
+import cairosvg
+import io
+import json
+from PIL import Image
+
+
+def gen_piece_img(piece_type: chess.PieceType, color: chess.Color, size):
+    piece = chess.Piece(piece_type, color)
+    svg = chess.svg.piece(piece)
+    png = cairosvg.svg2png(bytestring=svg, output_width=size, output_height=size, dpi=72)
+    return Image.open(io.BytesIO(png))
+
+
+def gen_piece_atlas(name='pieces', dpi=72, size=72):
+    all = Image.new('RGBA', size=(size*6,size*2))
+    atlas = {}
+    for color in [chess.BLACK, chess.WHITE]:
+        for piece_type in chess.PIECE_TYPES:
+            im = gen_piece_img(piece_type, color, size)
+            x, y = piece_type*size-size, color*size
+            all.paste(im, [x,y], im)
+            atlas[chess.Piece(piece_type, color).symbol()] = [x,all.size[1]-y-size,size,size]
+    fname = name + '.png'
+    all.save(fname, dpi=[dpi, dpi])
+    with open(name + '.atlas', 'w') as f:
+        json.dump({fname: atlas}, f)
+
+
+def gen_board_img(fname='board.png', dpi=72, square_size=72):
+    svg = chess.svg.board(board=None, size=8*square_size)
+    png = cairosvg.svg2png(bytestring=svg)
+    Image.open(io.BytesIO(png)).save(fname, dpi=[dpi, dpi])
+
+
+if __name__ == '__main__':
+    gen_piece_atlas()
+    gen_board_img()
